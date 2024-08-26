c25bc6e8055d6c787ca50a87f4d652dbd45bc579
Yarnsh <yarnsh@gmail.com>
2024-08-26 18:29:57 -0400

    fixes

--- SanctumPuzzle.gd
+++ SanctumPuzzle.gd
@@ -1,11 +1,14 @@
 extends Node3D
 
 @onready var death = $Death
+@onready var warning = $Warning
 
 func enable():
 	show()
 	death.set_deferred("monitoring", true)
+	warning.set_deferred("monitoring", true)
 
 func disable():
 	hide()
 	death.set_deferred("monitoring", false)
+	warning.set_deferred("monitoring", false)
--- global.gd
+++ global.gd
@@ -1,7 +1,7 @@
 extends Node
 
 var now = 0
-var debug = false
+const debug = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
